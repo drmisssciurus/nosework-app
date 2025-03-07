dd353423d88017f6ef209181a1dc9b149d96ccfe
Alina <ms.sciurus@gmail.com>
2025-03-07 10:56:05 +0200

    add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+// import favicon from './assets/favicon.ico';
+
+// const link = document.createElement('link');
+// link.rel = 'icon';
+// link.href = favicon;
+// document.head.appendChild(link);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
